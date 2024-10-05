06bcca0d48bb26dee5131f6a0fcfd6676c690074
LeeJaeHyun <myleje@gmail.com>
2024-10-05 17:29:39 +0900

    .

--- Assets/02_Scripts/03_RemoteConfig/RemoteConfigManager.cs
+++ Assets/02_Scripts/03_RemoteConfig/RemoteConfigManager.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 
 public class RemoteConfigManager : MonoBehaviour
@@ -9,9 +11,26 @@ public class RemoteConfigManager : MonoBehaviour
         await UnityServices.InitializeAsync();
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // RemoteConfig 로드 완료 콜백
+        RemoteConfigService.Instance.FetchCompleted += (response) =>
+        {
+            Debug.Log("로드 완료");
+            float moveSpeed = RemoteConfigService.Instance.appConfig.GetFloat("move_speed");
+            int bodyScale = RemoteConfigService.Instance.appConfig.GetInt("body_scale");
+
+            // Mummy 크기를 변경
+            GameObject.Find("Mummy").transform.localScale = Vector3.one * bodyScale;
+        };
+        await GetRemoteConfigAsync();
     }
 
-    // 데이터 로딩
     private struct userAttributes { };
+    private struct appAttributes { };
 
+    // Remote Config 데이터 로드
+    private async Task GetRemoteConfigAsync()
+    {
+        await RemoteConfigService.Instance.FetchConfigsAsync(new userAttributes(), new appAttributes());
+    }
 }
